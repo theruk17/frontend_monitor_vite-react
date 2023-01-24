ab7f3f8617b05741fe62c76e7e898389da303d11
theruk17 <therukza@hotmail.com>
2023-01-24 16:00:16 +0700

    fix2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import axios from 'axios'
-import Navbar from './Components/Navbar'
+import Navbar from './components/Navbar'
 import { NumericFormat } from 'react-number-format';
 import './App.css'
 
